2f52f47b2f3df56ad7689412292ce3da7c7beba9
pedrohoppe88 <pedrohenriquehoppe6@gmail.com>
2023-07-21 09:28:21 -0300

    first commit

---
+++ loginPage.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
+<link rel="stylesheet" href="CSS/style.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<title>Netflix - Watch TV Shows Online, Watch Movies Online</title>
+</head>
+<body>
+    <header class="showcase">
+        
+    <nav class="navbar ">
+  <div class="container-fluid">
+    <div class="navbar-header">
+        <div class="logo">
+        <img src="https://i.ibb.co/r5krrdz/logo.png">
+        </div>
+</nav>
+       
+    </header>
+
+
+</body>
+</html>
+
+
